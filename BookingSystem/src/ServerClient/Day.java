60118b09d4f6ef5d7e9e764582f1efbbab209359
Lance Huang <chvn0515@gmail.com>
2013-02-19 13:05:38 +0800

    trst

---
+++ BookingSystem/src/ServerClient/Day.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ServerClient;
+
+/**
+ *
+ * @author Lance
+ */
+public enum Day {
+    MONDDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
+}

478609289bb30ce7c5651aafe53f323fae4b9da4
Rikard Andersson <mail@rikardandersson.se>
2013-02-23 19:48:21 +0800

    Change the function names to better reflect their meaning

--- BookingSystem/src/ServerClient/ServerLog.java
+++ BookingSystem/src/ServerClient/ServerLog.java
@@ -11,27 +11,34 @@ import java.util.HashMap;
  */
 public class ServerLog {
     // log will contain the latest requests made
-    // The key will be the user making the request
-    // The value will be the request made
+    // The key will be the request ID
+    // The value will be the respons to the request
     private HashMap<String, String> log;
     
-    public void registerRequest(String requester, String request) {
+    public void registerRequest(String ID, String response) {
         // If this is the first registration, initiate the log
         if (log == null) {
             log = new HashMap<String, String>();
         }
-        // Handle the case where this request is already in the log
-        log.put(requester, request);
+        // @TODO - Handle the case where this request is already in the log
+        log.put(ID, response);
     }
     
-    // Returning the latest request made by the requester
-    public String lastRequest(String requester) {
+    // Returning a boolean indicating whether or not the request id is new
+    public boolean requestExists(String ID) {
+        if (log == null) {
+            return false;
+        } else {
+            return log.containsKey(ID);
+        }
+    }
+    
+    // Returning the respons for the requestID - returns null if there is no such ID
+    public String responsForRequest(String ID) {
         if (log == null) {
             return null;
-        } else if (log.containsKey(requester)) {
-            return log.get(requester);
         } else {
-            return null;
+            return log.get(ID);
         }
     }
 }

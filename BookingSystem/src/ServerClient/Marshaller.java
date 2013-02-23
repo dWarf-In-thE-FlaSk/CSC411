47ffd1d2941ea613363a546e0bc79712dacb1a41
Rikard Andersson <mail@rikardandersson.se>
2013-02-23 19:47:52 +0800

    Restructured the marshaller to be able to return message and requestID separatly

--- BookingSystem/src/ServerClient/Marshaller.java
+++ BookingSystem/src/ServerClient/Marshaller.java
@@ -10,7 +10,15 @@ public class Marshaller {
         // Placeholder function
         return null;
     }
-    public static byte[] marshall(String message) {
+    public static byte[] marshallMessage(String message) {
+        // Placeholder function
+        return null;
+    }
+    public static String getMessage(byte[] data) {
+        // Placeholder function
+        return null;
+    }
+    public static String getRequestID(byte[] data) {
         // Placeholder function
         return null;
     }

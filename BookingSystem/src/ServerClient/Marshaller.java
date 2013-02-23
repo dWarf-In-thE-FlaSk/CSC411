6c94a9e2983b9c2aa43d95ff1ef157e266608ae8
Rikard Andersson <mail@rikardandersson.se>
2013-02-23 17:48:30 +0800

    Added a static class, Marshaller, so far just a class with some placeholder functions.

---
+++ BookingSystem/src/ServerClient/Marshaller.java
@@ -0,0 +1,17 @@
+/*
+ */
+package ServerClient;
+
+/**
+ *
+ */
+public class Marshaller {
+    public static String unMarshall(byte[] data) {
+        // Placeholder function
+        return null;
+    }
+    public static byte[] marshall(String message) {
+        // Placeholder function
+        return null;
+    }
+}

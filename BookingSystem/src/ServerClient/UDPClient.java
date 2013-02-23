ae260afc40e4da76853031cc418d3c74fde29ccd
Raymond <s203502@gmail.com>
2013-02-23 21:09:32 +0800

    Client

--- BookingSystem/src/ServerClient/UDPClient.java
+++ BookingSystem/src/ServerClient/UDPClient.java
@@ -4,12 +4,49 @@
  */
 package ServerClient;
 
+import java.io.IOException;
+import java.net.*;
+
 /**
  *
  * @author
  */
-public interface UDPClient {
+public class UDPClient {
     
-    // We need some code here
+    private static int port;
+    private static InetAddress server;
+    private static String message;
+    public static void main(String args[]) throws Exception {
+     
+        DatagramSocket clientSocket=new DatagramSocket(); 
+        byte[] sendBuffer= new byte[512];
+        byte[] rcvBuffer= new byte[512];
+        sendBuffer=message.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, server, port); 
+        sendPkt(clientSocket,sendPacket);
+        DatagramPacket receivePacket=new DatagramPacket(rcvBuffer,rcvBuffer.length); 
+        message=receivePkt(clientSocket,receivePacket); 
+        
+    }
+    public UDPClient(String server, int port, String message) throws Exception {
+        this.server = InetAddress.getByName(server);
+        this.port=port;
+        this.message=message;
+           
+        
+    }
     
+    public static void sendPkt (DatagramSocket Socket, DatagramPacket packet ) throws IOException{
+        
+        Socket.send(packet);
+        
+    }// We need some code here
+    
+     static String receivePkt (DatagramSocket Socket, DatagramPacket packet) throws IOException{
+         
+        Socket.receive(packet);
+        String rcvMessage=new String(packet.getData()); 
+         
+        return(rcvMessage);
+    }
 }

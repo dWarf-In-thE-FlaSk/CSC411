ebeff9ddefb4f43286db6ba89d390dff2df3cfbd
Rikard Andersson <mail@rikardandersson.se>
2013-02-23 17:46:30 +0800

    Made a rough server implementation with a while loop listening for calls on UDP port 8008.

--- BookingSystem/src/ServerClient/BookingServer.java
+++ BookingSystem/src/ServerClient/BookingServer.java
@@ -4,6 +4,10 @@
  */
 package ServerClient;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.HashMap;
+
 /**
  * 
  * This class will contain the main method that runs and listens for calls
@@ -11,22 +15,39 @@ package ServerClient;
  * I will contain functions:
  * 
  * 
- * @author 
+ * @TODO - do we need to handle corrupted packets or splitting a message into 
+ * several packets?
  */
 public class BookingServer {
-    
-    // Variables:
-    // - An object of class Bookings
-    
+        
     public static void main(String args[]) throws Exception { // Specify later 
-        // Create sockets and initilize data structures that will contain bookings
-        // Listen for input from the client socket.
-        // When a call comes in:
-            // Unmarshall
-            // Switch with cases for each function
-            // Send call to function based on the received packet
-            // Return processed data from function
-        // Back to listening
+        // Create a socket for UPD-port 8008
+        DatagramSocket dgSocket = new DatagramSocket(8008);
+        BookingData bookingData = new BookingData();
+        ServerLog serverLog = new ServerLog();
+        byte[] data = new byte[1024];
+        DatagramPacket dgPacket = new DatagramPacket(data, data.length);
+
+        while(true) {
+            dgSocket.receive(dgPacket);
+            data = dgPacket.getData(); // This will be the array of bytes we need to unmarshal
+            String receivedMessage = Marshaller.unMarshall(data);
+            String returnMessage = doSomethingWithMessage(receivedMessage, bookingData);
+            data = Marshaller.marshall(returnMessage);
+            dgPacket.setData(data);
+            dgPacket.setAddress(dgPacket.getAddress());
+            dgSocket.send(dgPacket); // Throws IOException
+        }
     }
+    
+    // Placeholder class until we have some functions to call in bookingData
+    public static String doSomethingWithMessage(String message) {
+        switch (1) {
+            case 1: return "confirmationID";
+            default: return "Invalid command";
+        }
+                
+    }
+    
     
 }

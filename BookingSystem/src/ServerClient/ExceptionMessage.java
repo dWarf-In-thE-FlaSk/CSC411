69ea647f5acb65da3d5c8eeb76a00ce1cf64aac6
Rikard Andersson <mail@rikardandersson.se>
2013-03-20 14:34:13 +0800

    Implemented ExceptionMessage.

--- BookingSystem/src/ServerClient/ExceptionMessage.java
+++ BookingSystem/src/ServerClient/ExceptionMessage.java
@@ -4,6 +4,7 @@
  */
 package ServerClient;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,25 +12,56 @@ import java.util.List;
  * @author rikardandersson
  */
 public class ExceptionMessage implements Message {
+    
+    private static int messageType = -1;
+    private int requestID;
+    private String exceptionType;
+    private String exceptionMessage;
 
-    @Override
-    public List<String> serializeMessageContent() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public String getExceptionType() {
+        return exceptionType;
     }
 
+    public String getExceptionMessage() {
+        return exceptionMessage;
+    }
+    
+    public void setExceptionMessage(String exceptionMessage) {
+        this.exceptionMessage = exceptionMessage;
+    }
+    
+    public void setExceptionType(String exceptionType) {
+        this.exceptionType = exceptionType;
+    }
+    
     @Override
     public int getMessageType() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return messageType;
     }
 
     @Override
     public int getRequestID() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public void setRequestID() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return requestID;
     }
     
+    @Override
+    public void setRequestID(int requestID) {
+        this.requestID = requestID;
+    }
+    
+    @Override
+    public List<String> serializeMessageContent() {
+        List<String> serializedContent = new ArrayList<String>();
+        serializedContent.add(String.valueOf(this.getMessageType()));
+        serializedContent.add(String.valueOf(this.getRequestID()));
+        serializedContent.add(exceptionType);
+        serializedContent.add(exceptionMessage);
+        return serializedContent;
+    }
+    @Override
+    public void unserializeAndSetMessageContent(List<String> serializedMessageContent) {
+        this.setRequestID(Integer.parseInt(serializedMessageContent.get(1)));
+        this.setExceptionType(serializedMessageContent.get(2));
+        this.setExceptionMessage(serializedMessageContent.get(3));
+    }
 }

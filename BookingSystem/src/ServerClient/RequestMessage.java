57c7f7ad3ef7b58f2ade63e93005be3ac9951f93
Rikard Andersson <mail@rikardandersson.se>
2013-03-20 14:54:58 +0800

    Implemented RequestMessage

--- BookingSystem/src/ServerClient/RequestMessage.java
+++ BookingSystem/src/ServerClient/RequestMessage.java
@@ -4,7 +4,10 @@
  */
 package ServerClient;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -12,9 +15,19 @@ import java.util.List;
  */
 public class RequestMessage implements Message {
 
+    private static int messageType = 1;
+    private int requestID;
+    private String request;
+    private Map<String, String> attributes;
+    
     @Override
     public List<String> serializeMessageContent() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        List<String> serializedContent = new ArrayList<String>();
+        serializedContent.add(String.valueOf(this.getMessageType()));
+        serializedContent.add(String.valueOf(this.getRequestID()));
+        
+        
+        return serializedContent;
     }
 
     @Override
@@ -26,10 +39,26 @@ public class RequestMessage implements Message {
     public int getRequestID() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
+    
+    @Override
+    public void setRequestID(int requestID) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     @Override
-    public void setRequestID() {
+    public void unserializeAndSetMessageContent(List<String> serializedMessageContent) {
         throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public String getAttribute(String key) {
+        return attributes.get(key);
+    }
+    
+    public void setAttribute(String key, String value) {
+        if (attributes == null) {
+            attributes = new HashMap<String, String>();
+        }
+        attributes.put(key, value);
     }
     
 }

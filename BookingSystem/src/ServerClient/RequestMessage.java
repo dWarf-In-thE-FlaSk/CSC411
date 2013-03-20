638af2311da7e29424903490a5a8e17772831b70
Rikard Andersson <mail@rikardandersson.se>
2013-03-20 17:26:01 +0800

    RequestMessage update

--- BookingSystem/src/ServerClient/RequestMessage.java
+++ BookingSystem/src/ServerClient/RequestMessage.java
@@ -6,6 +6,7 @@ package ServerClient;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -17,17 +18,47 @@ public class RequestMessage implements Message {
 
     private static int messageType = 1;
     private int requestID;
-    private String request;
+    private int request;
     private Map<String, String> attributes;
-    
+
     @Override
     public List<String> serializeMessageContent() {
         List<String> serializedContent = new ArrayList<String>();
         serializedContent.add(String.valueOf(this.getMessageType()));
         serializedContent.add(String.valueOf(this.getRequestID()));
+        serializedContent.add(String.valueOf(this.getRequest()));
         
-        
+        for (Map.Entry<String, String> entry : attributes.entrySet()) {
+            serializedContent.add(entry.getKey());
+            serializedContent.add(entry.getValue());
+        }
         return serializedContent;
+    }
+    
+    @Override
+    public void unserializeAndSetMessageContent(List<String> serializedMessageContent) {
+        this.setRequestID(Integer.parseInt(serializedMessageContent.get(1)));
+        this.setRequest(Integer.parseInt(serializedMessageContent.get(2)));
+        
+        if (attributes == null) {
+            attributes = new HashMap<String, String>();
+        }
+        
+        Iterator<String> iter = serializedMessageContent.subList(3, serializedMessageContent.size()).iterator();
+        while(iter.hasNext()) {
+            String key = iter.next();
+            if(iter.hasNext()) {
+                this.setAttribute(key, iter.next());
+            }
+        }
+    }   
+
+    public void setRequest(int request) {
+        this.request = request;
+    }
+
+    public int getRequest() {
+        return request;
     }
 
     @Override
@@ -42,11 +73,6 @@ public class RequestMessage implements Message {
     
     @Override
     public void setRequestID(int requestID) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public void unserializeAndSetMessageContent(List<String> serializedMessageContent) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
     

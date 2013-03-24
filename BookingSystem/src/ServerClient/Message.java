f0831590af63028763659f46e7916928b625302f
Rikard Andersson <mail@rikardandersson.se>
2013-03-24 10:53:49 +0800

    Added comments to explain the Message interface.

--- BookingSystem/src/ServerClient/Message.java
+++ BookingSystem/src/ServerClient/Message.java
@@ -1,21 +1,27 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
  */
 package ServerClient;
 
 import java.util.List;
 
 /**
- *
- * @author rikardandersson
+ * This is an interface implemented by classes that represent messages being 
+ * passed between client and server.
+ * 
+ * @author Rikard Andersson
  */
 public interface Message {
+    // This will return a flattened version of the content of the Message
     public List<String> serializeMessageContent();
+    // This will build the Message from the flattened version of the content return by the above method.
     public void unserializeAndSetMessageContent(List<String> serializedMessageContent);
+    // Every message will have a RequestID created and appended by the client.
     public int getRequestID();
     public void setRequestID(int requestID);
+    // The MessageType will indicate what kind of message this is. It needs to be unique for each new implementation of Message.
     public int getMessageType();
+    
+    // equals() and hashCode() needs to be implemented for the ServerLog to work.
     @Override
     public boolean equals(Object o);
     @Override

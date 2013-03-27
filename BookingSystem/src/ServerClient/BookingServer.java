9221e38e448498464887b7c92484a1d3eb0b02ac
Rikard Andersson <mail@rikardandersson.se>
2013-03-27 16:11:02 +0800

    Too much lost messages! need less! changed to achieve this.

--- BookingSystem/src/ServerClient/BookingServer.java
+++ BookingSystem/src/ServerClient/BookingServer.java
@@ -86,8 +86,8 @@ public class BookingServer {
         
         // A message is lost every [lossFrequency] message received
         int iterations = 0;
-        int requestLossFreq = 5;
-        int responseLossFreq = 7;
+        int requestLossFreq = 7;
+        int responseLossFreq = 11;
         
         while(true) {
             // For the message loss
